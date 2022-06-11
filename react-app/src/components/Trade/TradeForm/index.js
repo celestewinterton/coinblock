6bd29192c0cefe2ee65e3ef7adaa4a18651bb39b
Celeste Winterton <celestewinterton@gmail.com>
2022-06-11 16:39:19 -0700

    styled trade form

--- react-app/src/components/Trade/TradeForm/index.js
+++ react-app/src/components/Trade/TradeForm/index.js
@@ -19,7 +19,7 @@ const TradeForm = ({showModal, setShowModal}) => {
     const coins = useSelector(state => state.crypto)
     const ownedCoins = Object.values(coins).filter(coin => Object.keys(user.balances).includes(`${coin.id}`))
     const [errors, setErrors] = useState({});
-    const [amount, setAmount] = useState()
+    const [amount, setAmount] = useState('')
     const [cryptoId, setCryptoId] = useState(1)
     const [bank, setBank] = useState("Cash (USD)")
     const [type, setType] = useState("buy")
@@ -83,58 +83,66 @@ const TradeForm = ({showModal, setShowModal}) => {
                 <NavLink to="#" className="sell-tab" style={type === "sell" ? {color: "#0052FF", borderBottom: "none"} : null} onClick={e => setType("sell")}>Sell</NavLink>
                 <NavLink to="#" className="fund-tab" style={type === "transfer" ? {color: "#0052FF", borderBottom: "none"} : null} onClick={e => setType("transfer")}>Add Funds</NavLink>
             </div>
-            <form autoComplete="off" onSubmit={handleSubmit}>
-                <div>{type === "transfer" ? null : `Current Price: ${currency(price)}`}</div>
-                <div>{type === "sell" ? `${coins[cryptoId]?.name} qty: ${round(user.balances[cryptoId])}, value: ${currency(user.balances[cryptoId]*price)}` : `Cash Balance: ${currency(user.balances.cash)}`}</div>
-                <div>
+            <form className="center column" autoComplete="off" onSubmit={handleSubmit}>
+                <div className="amount-input-container">
+                    <label className="row amount-label" >$
                     <input
+                        className="amount-input"
+                        // width={`${2 * 10}px`}
                         type='text'
                         value={amount}
                         required
                         onChange={(e) => setAmount(e.target.value)}
-                        placeholder='$0'></input>
+                        placeholder='0'></input>
                     <div className="form-errors">
                         {errors.amount && <p>{errors.amount}</p>}
                     </div>
-                </div>
-                <div>
-                    {type != "transfer" &&
-                    <label>{type === "buy" ? "Buy" : "Sell"}
-                    <select
-                        type='text'
-                        value={cryptoId}
-                        required
-                        onChange={(e) => setCryptoId(e.target.value)}
-                        placeholder='$0'>
-                            {type === "buy" ? Object.values(coins).map(coin => (<option key={coin.id} value={coin.id}>{coin.name}</option>))
-                            : Object.values(ownedCoins).map(coin => (<option key={coin.id} value={coin.id}>{coin.name}</option>))}
-                    </select>
-                    </label>}
-                    {type === "transfer" &&
-                    <label>From
-                    <select
-                        type='text'
-                        value={cryptoId}
-                        required
-                        onChange={(e) => setBank(e.target.value)}>
-                            <option key="bank1" value="Bank of Satoshi">Bank of Satoshi</option>
-                            <option key="bank2" value="Bank of America">Bank of America</option>
-                            <option key="bank3" value="Chase">Chase</option>
-                    </select>
-                    </label>}
-                </div>
-                <div>
-                    <label>{type === "buy" ? "Pay with" : "Add to"}
-                    <select
-                        type='text'
-                        required>
-                            <option>Cash (USD)</option>
-                    </select>
                     </label>
                 </div>
-                <button className="wide top-margin" type="submit">{type === "transfer" ? "Deposit Funds" : type === "buy" ? `Buy ${coins[cryptoId]?.name}` : `Sell ${coins[cryptoId]?.name}`}</button>
+                <div className="muted1">{type === "transfer" ? null : `1 ${coins[cryptoId]?.symbol.toUpperCase()} ≈ ${currency(price)}`}</div>
+                <div className="muted1">Your current cash balance is {currency(user.balances.cash)}</div>
+                <div className="select-inputs">
+                    {type != "transfer" &&
+                    <div className="select-field">
+                        <label className="muted1">{type === "buy" ? "Buy" : "Sell"}</label>
+                        <select
+                            type='text'
+                            value={cryptoId}
+                            required
+                            onChange={(e) => setCryptoId(e.target.value)}
+                            placeholder='$0'>
+                                {type === "buy" ? Object.values(coins).map(coin => (<option key={coin.id} value={coin.id}>{coin.name}</option>))
+                                : Object.values(ownedCoins).map(coin => (<option key={coin.id} value={coin.id}>{coin.name}</option>))}
+                        </select>
+                        </div>}
+                    {type === "transfer" &&
+                    <div className="select-field">
+                        <label className="muted1">From</label>
+                        <select
+                            type='text'
+                            value={cryptoId}
+                            required
+                            onChange={(e) => setBank(e.target.value)}>
+                                <option key="bank1" value="Bank of Satoshi">Bank of Satoshi</option>
+                                <option key="bank2" value="Bank of America">Bank of America</option>
+                                <option key="bank3" value="Chase">Chase</option>
+                        </select>
+                    </div>}
+                    <div className="select-field-last">
+                        <label className="muted1">{type === "buy" ? "Pay with" : "Add to"}</label>
+                        <select
+                            type='text'
+                            required>
+                                <option>Cash (USD)</option>
+                        </select>
+                    </div>
+                </div>
+                <button className="wide top-margin large-button" type="submit">{type === "transfer" ? "Deposit Funds" : type === "buy" ? `Buy ${coins[cryptoId]?.name}` : `Sell ${coins[cryptoId]?.name}`}</button>
+                {type != "transfer" && <div className="top-margin muted1 row space-between">
+                    <div>{coins[cryptoId]?.symbol.toUpperCase()} balance</div>
+                    <div>{user.balances[cryptoId] ? round(user.balances[cryptoId]) : 0} {coins[cryptoId]?.symbol.toUpperCase()} ≈ {user.balances[cryptoId] ? currency(user.balances[cryptoId]*price) : "$0"}</div>
+                </div>}
             </form>
-            {true && <form></form>}
         </div>
     );
 };
